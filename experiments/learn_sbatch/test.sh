b5e9e866fd2a30719bb3dd9ce96d5928245da3a0
Boxiang Liu <jollier.liu@gmail.com>
2020-10-01 13:10:57 -0700

    modified:   experiments/learn_sbatch/test.sh 	modified:   preprocess/split_sent.sh

--- experiments/learn_sbatch/test.sh
+++ experiments/learn_sbatch/test.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
-sleep 120
-python3 preprocess/split_sent.py --in_fn $in_fn --out_fn $out_fn
+#SBATCH --gres=gpu:1
+#SBATCH --job-name=test1
+#SBATCH --output=test1.out
+python3 preprocess/split_sent.py --in_fn ../processed_data/preprocess/abstract/pubmed19n0971.txt --out_fn test971.txt
--- preprocess/split_sent.sh
+++ preprocess/split_sent.sh
@@ -2,17 +2,21 @@ in_dir=../processed_data/preprocess/abstract/
 out_dir=../processed_data/preprocess/sentence/
 mkdir -p $out_dir
 
-split_sent(){
+make_sub(){
     in_fn=$1
     out_dir=$2
-    out_fn="$out_dir/`basename $in_fn`"
-    echo -e "INPUT\t$in_fn"
-    echo -e "OUTPUT\t$out_fn"
-
-    python3 preprocess/split_sent.py --in_fn $in_fn --out_fn $out_fn
+    base=`basename $in_fn .txt`
+    echo \#\!/bin/sh > $out_dir/${base}.sub
+    echo "#SBATCH --gres=gpu:1" >> $out_dir/${base}.sub
+    echo "#SBATCH --job-name=$base" >> $out_dir/${base}.sub 
+    echo "#SBATCH --output=$out_dir/${base}.out" >> $out_dir/${base}.sub
+    echo "python3 preprocess/split_sent.py --in_fn $in_fn --out_fn $out_dir/${base}.txt" >> $out_dir/${base}.sub
 }
+export -f make_sub
 
-export -f split_sent
+parallel -j 10 make_sub {} {} ::: `ls $in_dir/pubmed*.txt` ::: $out_dir
 
-parallel -j 10 --joblog $out_dir/split_sent.log split_sent {} {} ::: `ls $in_dir/pubmed*.txt` ::: $out_dir
 
+for sub in `ls $out_dir/pubmed*.sub`; do
+    sbatch -p TitanXx8 $sub
+done
