50fbeb0bfdfc8f4ccadcf50f286550763f6adbe5
Boxiang Liu <jollier.liu@gmail.com>
2020-10-01 17:22:59 -0700

    modified:   preprocess/extract_abbrev.py 	new file:   preprocess/extract_abbrev.sh

--- preprocess/extract_abbrev.py
+++ preprocess/extract_abbrev.py
@@ -1,22 +1,27 @@
 import re
 import os
+import click
 
-in_fn = "../processed_data/preprocess/sentence/pubmed19n0972.txt"
-out_dir = "../processed_data/preprocess/abbrev/"
-os.makedirs(out_dir, exist_ok=True)
-out_fn = os.path.join(out_dir, "pubmed19n0972.txt")
 
-with open(in_fn) as fin, open(out_fn, "w") as fout:
-    for line in fin:
-        split_line = line.strip().split("|")
-        pmid = split_line[0]
-        typ = split_line[1]
-        line_no = split_line[2]
-        text = split_line[3]
+@click.command()
+@click.option("--in_fn", type=str, help="Input file.")
+@click.option("--out_fn", type=str, help="Output file.")
+def main(in_fn, out_fn):
+    with open(in_fn) as fin, open(out_fn, "w") as fout:
+        for line in fin:
+            split_line = line.strip().split("|")
+            pmid = split_line[0]
+            typ = split_line[1]
+            line_no = split_line[2]
+            text = split_line[3]
 
-        for m in re.finditer("\((.*?)\)", text):
-            m_text = m.group(1)
-            start = m.start(1)
-            end = m.end(1)
-            out = f"{pmid}|{typ}|{line_no}|{start},{end}|{m_text}\n"
-            fout.write(out)
+            for m in re.finditer("\((.*?)\)", text):
+                m_text = m.group(1)
+                start = m.start(1)
+                end = m.end(1)
+                out = f"{pmid}|{typ}|{line_no}|{start},{end}|{m_text}\n"
+                fout.write(out)
+
+
+if __name__ == "__main__":
+    main()
---
+++ preprocess/extract_abbrev.sh
@@ -0,0 +1,16 @@
+out_dir="../processed_data/preprocess/abbrev/"
+mkdir -p $out_dir
+
+extract_abbrev(){
+    in_fn=$1
+    out_dir=$2
+
+    echo -e "INPUT\t$in_fn"
+    out_fn="$out_dir/`basename $in_fn`"
+    echo -e "OUTPUT\t$out_fn"
+    python3 preprocess/extract_abbrev.py --in_fn $in_fn --out_fn $out_fn
+
+}
+export -f extract_abrev
+
+parallel -j 35 --joblog ../processed_data/preprocess/abstract/extract_abrev.log extract_abbrev {} {} ::: `ls ../processed_data/preprocess/sentence/pubmed*.txt` ::: $out_dir
