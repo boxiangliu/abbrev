575f2124d8c9f91fea43bb55c6bdb381ca789fd0
Boxiang Liu <jollier.liu@gmail.com>
2020-10-02 18:26:03 -0700

    modified:   preprocess/extract_abbrev.py

--- preprocess/extract_abbrev.py
+++ preprocess/extract_abbrev.py
@@ -15,11 +15,23 @@ def main(in_fn, out_fn):
             line_no = split_line[2]
             text = split_line[3]
 
-            for m in re.finditer("\((.*?)\)", text):
+            for m in re.finditer(" \((.*?)[\),\.\?:\'\"]", text):
                 m_text = m.group(1)
                 start = m.start(1)
                 end = m.end(1)
-                out = f"{pmid}|{typ}|{line_no}|{start},{end}|{m_text}|{text}\n"
+
+                abb_type = 0 
+                if " " in m_text: # space
+                    abb_type = 1
+                elif not re.search("[a-zA-Z]", m_text): # no letter
+                    abb_type = 2
+                elif re.match("^(IX|IV|V?I{0,3})$", m_text.upper()): # Roman numeral
+                    abb_type = 3
+                elif re.match("^[a-z]$", m_text): # only lowercase letter
+                    abb_type = 4
+                elif re.search("[pP]<0", m_text): # p-value 
+                    abb_type = 5
+                out = f"{pmid}|{typ}|{line_no}|{start},{end}|{abb_type}|{m_text}|{text}\n"
                 fout.write(out)
 
 
