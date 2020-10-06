c72475353b84899d62c424a8213b663daa5c67b0
Boxiang Liu <jollier.liu@gmail.com>
2020-10-06 12:08:20 -0700

    small fix

--- preprocess/extract_abbrev.py
+++ preprocess/extract_abbrev.py
@@ -1,12 +1,20 @@
 import re
 import os
 import click
+import pycountry
+
+
+def get_countries():
+    countries = [x.name for x in list(pycountry.countries)]
+    return countries
 
 
 @click.command()
 @click.option("--in_fn", type=str, help="Input file.")
 @click.option("--out_fn", type=str, help="Output file.")
 def main(in_fn, out_fn):
+    countries = get_countries()
+
     with open(in_fn) as fin, open(out_fn, "w") as fout:
         for line in fin:
             split_line = line.strip().split("|")
@@ -29,12 +37,18 @@ def main(in_fn, out_fn):
                     abb_type = 3
                 elif re.match("^[a-z]$", m_text): # only lowercase letter
                     abb_type = 4
-                elif re.search("[pP][<>=]0", m_text): # p-value 
+                elif re.search("[pP][<>=≤≥⩽⩾]", m_text): # p-value 
                     abb_type = 5
                 elif re.search("[nN]=", m_text): # sample size
                     abb_type = 6
-                elif re.search("=", m_text): # definition
+                elif re.search("[=±]", m_text): # definition and unit
                     abb_type = 7
+                elif re.search("°C", m_text): # temperature
+                    abb_type = 8
+                elif re.search("(\.com|\.gov)", m_text): # URLs
+                    abb_type = 9
+                elif m_text in countries:
+                    abb_type = 10
 
                 out = f"{pmid}|{typ}|{line_no}|{start},{end}|{abb_type}|{m_text}|{text}\n"
                 fout.write(out)
