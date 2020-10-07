7c1387ed013d384c9f809c6d67c899fbcc9cb6a3
Boxiang Liu <jollier.liu@gmail.com>
2020-10-07 00:18:55 -0700

    modified:   preprocess/count_abbrev.py

--- preprocess/count_abbrev.py
+++ preprocess/count_abbrev.py
@@ -1,5 +1,5 @@
 import glob
-from collections import Counter
+from collections import Counter, defaultdict
 import matplotlib
 matplotlib.use("TkAgg")
 import matplotlib.pyplot as plt
@@ -17,19 +17,58 @@ for fn in glob.glob(f"{in_dir}/pubmed*.txt"):
     with open(fn) as fin:
         for line in fin:
             split_line = line.strip().split("|")
-            abbrev = split_line[4]
-            counter[abbrev] += 1
+            abb_type = split_line[4]
+            if abb_type == "0":
+                abbrev = split_line[5]
+                counter[abbrev] += 1
 
 
-with open(f"{out_dir}/count.pkl", "wb") as fout:
-    pickle.dump(counter, fout)
+counter_filt = {x: count for x, count in counter.items() if count > 1}
+counter_sort = sorted(counter.items(), key=lambda pair: pair[1], reverse=True)
 
 
-with open(f"{out_dir}/count.pkl", "rb") as fin:
-    counter = pickle.load(fin)
+with open(f"{out_dir}/count.tsv", "w") as fout:
+    for x, count in counter_sort:
+        fout.write(f"{x}\t{count}\n")
+
+
+with open(f"{out_dir}/count.tsv", "r") as fin:
+    counter_sort = list()
+    for line in fin:
+        split_line = line.strip().split("\t")
+        abbrev = split_line[0]
+        count = int(split_line[1])
+        counter_sort.append((abbrev, count))
+
+
+zipf = defaultdict(list)
+for i, (x, count) in enumerate(counter_sort):
+    zipf["rank"].append(i+1)
+    zipf["freq"].append(count)
 
 
 plt.ion()
+plt.show()
 plt.xscale("log")
-plt.hist(counter.values(), log=True, bins=1000)
-plt.savefig(f"{out_dir}/count_hist.pdf")
+plt.yscale("log")
+plt.plot(zipf["rank"], zipf["freq"])
+plt.xlabel("log(rank)")
+plt.ylabel("log(frequency)")
+plt.tight_layout()
+plt.savefig(f"{out_dir}/rank_freq.pdf")
+
+
+total_count = 0
+cum_count = [0]
+for x, count in counter_sort:
+    total_count += count
+    cum_count.append(cum_count[-1]+count)
+
+
+cutoff = 0.8
+for i, count in enumerate(cum_count):
+    if count > total_count * cutoff:
+        print(f"Top {i} words account for {cutoff*100}% of all occurances.")
+        break
+
+# Top 42278 words account for 80.0% of all occurances.
