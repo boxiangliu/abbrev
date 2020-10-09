85e8ed9a73a90d4fbf4c0d87ef910ce7a351b362
Boxiang Liu <jollier.liu@gmail.com>
2020-10-08 23:55:53 -0700

    small fix

--- preprocess/ab3p/summarize_ab3p.py
+++ preprocess/ab3p/summarize_ab3p.py
@@ -2,6 +2,7 @@ import os
 import glob
 from collections import defaultdict
 import pandas as pd
+import re
 
 in_dir = "../processed_data/preprocess/ab3p/run_ab3p/"
 out_dir = "../processed_data/preprocess/ab3p/summarize_ab3p/"
@@ -13,16 +14,20 @@ for fn in glob.glob(f"{in_dir}/pubmed*.out"):
     with open(fn, encoding="ISO-8859-1") as fin:
         for line in fin:
             try:
-                if line.startswith(">"):
+                if re.match(">[0-9]+\|[at]\|[0-9]+"):
                     split_line = line.strip().replace(">","").split("|")
-                    container["pmid"].append(int(split_line[0]))
-                    container["type"].append(split_line[1])
-                    container["sent"].append(int(split_line[2]))
+                    pmid = int(split_line[0])
+                    typ = split_line[1]
+                    sent = int(split_line[2])
+
                 if line.startswith("  "):
                     split_line = line.strip().split("|")
                     container["sf"].append(split_line[0])
                     container["lf"].append(split_line[1])
                     container["score"].append(float(split_line[2]))
+                    container["pmid"].append(pmid)
+                    container["type"].append(typ)
+                    container["sent"].append(sent)
             except:
                 print(line)
 
