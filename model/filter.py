7c756141ffc9f26b2f0d60953d37e57b3db86e03
Boxiang Liu <jollier.liu@gmail.com>
2020-10-30 18:06:00 -0700

    small fix

--- model/filter.py
+++ model/filter.py
@@ -3,6 +3,6 @@ import re
 for line in sys.stdin:
     if line.startswith("  "):
         sf = line.strip().split("|")[0]
-        match = re.match("[-+]?([0-9]*\.[0-9]+|[0-9]+)[ %]?", sf)
+        match = re.match("[-+]?([0-9]*\.[0-9]+|[0-9]+)[ %]", sf)
         if match is not None:
             sys.stdout.write(sf + "\n")
