425c70f1b27de3dbfca016ab48f6320eff7d718d
Boxiang Liu <jollier.liu@gmail.com>
2020-10-30 18:12:08 -0700

    small fix

--- model/filter.py
+++ model/filter.py
@@ -8,11 +8,17 @@ for line in sys.stdin:
             pass
             # sys.stdout.write(sf + "\n")
 
-        m2 = re.match("[0-9]+$", sf)
+        m2 = re.match("[0-9]+", sf)
         if m2 is not None:
             pass
             # sys.stdout.write(sf + "\n")
 
+
         m3 = re.match("^(IX|IV|V?I{0,3})$", sf.upper())
         if m3 is not None:
+            pass
+            # sys.stdout.write(sf + "\n")
+
+        m4 = re.match("[A-Za-z]", sf)
+        if m4 is None:
             sys.stdout.write(sf + "\n")
