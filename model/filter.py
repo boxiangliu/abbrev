153ec726ddf284b717b69a6d1e6d28b15badc4cf
Boxiang Liu <jollier.liu@gmail.com>
2020-10-30 18:33:01 -0700

    small fix

--- model/filter.py
+++ model/filter.py
@@ -8,30 +8,37 @@ for line in sys.stdin:
         fltr = False
         m1 = re.search("[-+]?([0-9]*\.[0-9]+|[0-9]+)[ %]", sf)
         if m1 is not None:
+            print(1)
             fltr = True
 
         m2 = re.match("[0-9]+$", sf)
         if m2 is not None:
+            print(2)
             fltr = True
 
         m3 = re.match("^(IX|IV|V?I{0,3})$", sf.upper())
         if m3 is not None:
+            print(3)
             fltr = True
 
         m4 = re.search("[A-Za-z]", sf)
         if m4 is None:
+            print(4)
             fltr = True
 
         m5 = re.findall(" ([\(\[](?>[^\(\)\[\]]+|(?1))*[\)\]])", sf)
         if len(m5) > 0:
+            print(5)
             fltr = True
 
         m6 = re.search("[<>=≤≥⩽⩾±]", sf)
         if m6 is not None:
+            print(6)
             fltr = True
 
         m7 = re.search("[a-g]$", sf)
         if m7 is not None:
+            print(7)
             fltr = True
 
         if fltr == True:
