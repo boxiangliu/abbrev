b1ce03a258fc191b0f0af4daf6140f7d2076aa7e
Boxiang Liu <jollier.liu@gmail.com>
2020-11-04 11:52:31 -0800

    fixed line2fasta.py

--- preprocess/line2fasta.py
+++ preprocess/line2fasta.py
@@ -2,5 +2,6 @@ import sys
 
 for line in sys.stdin:
     split_line = line.split("|", 3)
-    out_line = f">{split_line[:3]}\n{split_line[3]}"
+    header = "|".join(split_line[:3])
+    out_line = f">{header}\n{split_line[3]}"
     sys.stdout.write(out_line)
