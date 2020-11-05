13df0b3fcfd3f49fd1ba1a4df470755494a8a358
Boxiang Liu <jollier.liu@gmail.com>
2020-11-05 10:40:25 -0800

    small fix

--- model/3D_freq.py
+++ model/3D_freq.py
@@ -1,23 +1,31 @@
 import sys
 from collections import Counter
 from tqdm import tqdm
+import click
 
-freq = Counter()
-for line in tqdm(sys.stdin):
+@click.command()
+@click.option("--topk", type=int, default=5, help="Only output top K results.")
+def main(topk):
+    freq = Counter()
+    for line in tqdm(sys.stdin):
 
-    if line.startswith("  "):
-        split_line = line.strip().split("|")
-        comment = split_line[3]
-        if comment == "input":
-            continue
-        else:
+        if line.startswith("  "):
+            split_line = line.strip().split("|")
+            comment = split_line[3]
+            if comment == "input": continue
+
             sf = split_line[0]
             lf = split_line[1]
-            rank = split_line[3].replace("bqaf","")
+            rank = int(split_line[3].replace("bqaf",""))
+            if rank > topk: continue
+
             freq[(sf, lf, rank)] += 1
 
-for k, freq in freq.items():
-    sf = k[0]
-    lf = k[1]
-    rank = k[2]
-    sys.stdout.write(f"{sf}\t{lf}\t{rank}\t{freq}\n")
+    for k, freq in freq.items():
+        sf = k[0]
+        lf = k[1]
+        rank = k[2]
+        sys.stdout.write(f"{sf}\t{lf}\t{rank}\t{freq}\n")
+
+if __name__ == "__main__":
+    main()
