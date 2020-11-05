862189bddc93cfc718c1c30ee8d5874dfc156a5e
Boxiang Liu <jollier.liu@gmail.com>
2020-11-05 11:44:27 -0800

    small fix

--- preprocess/trng_PMIDs.py
+++ preprocess/trng_PMIDs.py
@@ -4,24 +4,24 @@ from tqdm import tqdm
 
 
 @click.command()
-@click.option("--med1250_PMID", type=str, help="path to MED1250 PubMed IDs.")
-@click.option("--BioText_PMID", type=str, help="path to BioText PubMed IDs.")
-def main(med1250_PMID, BioText_PMID):
-    eval_PMID = set()
+@click.option("--med1250_pmid", type=str, help="path to MED1250 PubMed IDs.")
+@click.option("--biotext_pmid", type=str, help="path to BioText PubMed IDs.")
+def main(med1250_pmid, biotext_pmid):
+    eval_pmid = set()
 
-    with open(med1250_PMID) as f:
+    with open(med1250_pmid) as f:
         for line in f:
-            eval_PMID.add(line.strip())
+            eval_pmid.add(line.strip())
 
-    with open(BioText_PMID) as f:
+    with open(BioText_pmid) as f:
         for line in f:
-            eval_PMID.add(line.strip())
+            eval_pmid.add(line.strip())
 
     for line in tqdm(sys.stdin):
         line = line.strip()
         if line == "pmid":
             continue
-        if line in eval_PMID:
+        if line in eval_pmid:
             continue
         sys.stdout.write(line + "\n")
 
