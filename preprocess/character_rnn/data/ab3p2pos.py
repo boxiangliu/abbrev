a1078b38272c2032a9b826cd9849c395fba1a7ff
Boxiang Liu <jollier.liu@gmail.com>
2020-11-12 01:03:47 -0800

    updated ab3p2npos

--- preprocess/character_rnn/data/ab3p2pos.py
+++ preprocess/character_rnn/data/ab3p2pos.py
@@ -4,6 +4,6 @@ for line in sys.stdin:
     split_line = line.strip().split("\t")
     score = float(split_line[2])
     freq = int(split_line[4])
-    if score + 0.005 * freq > 0.995:
-        out_line = "\t".join(split_line[:5])
+    if (freq > 1) and (score + 0.005 * freq) >= 1:
+        out_line = "\t".join(split_line[:2])
         sys.stdout.write(out_line + "\n")
