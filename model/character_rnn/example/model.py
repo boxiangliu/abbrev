8610844d458c59e0c693ab074a5702bc74a45bef
Boxiang Liu <jollier.liu@gmail.com>
2020-11-16 15:08:46 -0800

    updated model.py

--- model/character_rnn/example/model.py
+++ model/character_rnn/example/model.py
@@ -10,7 +10,7 @@ class RNN(nn.Module):
 
         self.i2h = nn.Linear(input_size + hidden_size, hidden_size)
         self.i2o = nn.Linear(input_size + hidden_size, output_size)
-        self.softmax = nn.LogSoftmax()
+        self.softmax = nn.LogSoftmax(dim=0)
 
     def forward(self, input, hidden):
         combined = torch.cat((input, hidden), 1)
