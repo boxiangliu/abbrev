0f1453299f941eeb00ee3b90bfccfbd7b967caf8
Boxiang Liu <jollier.liu@gmail.com>
2020-11-18 18:44:08 -0800

    updated data.py

--- model/character_rnn/data.py
+++ model/character_rnn/data.py
@@ -4,6 +4,7 @@ from unidecode import unidecode
 import os
 from torch.utils.data import Dataset, DataLoader
 from collections import defaultdict
+from pathlib import Path
 
 
 all_letters = string.printable
@@ -88,7 +89,67 @@ def pad_seq(samples):
     return torch.tensor(labels), padded_seqs, seq_lens
 
 
-dataset = SFLFPairs("test")
-dataloader = DataLoader(dataset, batch_size=4, shuffle=True, num_workers=1, collate_fn = pad_seq)
-i, (labels, padded_seqs, seq_lens) = next(enumerate(dataloader))
+class SF(nn.Module):
+    """Classify short form into valid and invalid"""
 
+    def __init__(self, flist, exclude=set()):
+        """Args:
+            flist (list): a list of files.
+            exclude (set): a set of short forms to exclude.
+                This can be used to remove short forms in eval set.
+        """
+        self.flist = flist
+        self.data = self.read_files(flist, exclude)
+        # empty string means unknown
+        self.characters = [""] + list(string.printable)
+        self.n_characters = len(self.characters)
+
+    def __len__(self):
+        return len(self.data["seq"])
+
+    def __getitem__(self, idx):
+        seq = self.seq2tensor(self.data["seq"][idx])
+        label = int(self.data["label"][idx])
+        return seq, label
+
+    def read_files(self, flist, exclude):
+        seqs = []
+        labels = []
+        for fn in flist:
+            with open(fn) as f:
+                for line in f:
+                    split_line = line.strip().split("\t")
+                    if len(split_line) == 2:
+                        if split_line[0] not in exclude:
+                            seqs.append(split_line[0])
+                            labels.append(split_line[1])
+        return {"seq": seqs, "label": labels}
+
+    def seq2tensor(self, seq):
+        tensor = torch.zeros(len(seq), self.n_characters)
+        for i, character in enumerate(seq):
+            ascii_char = unidecode(character)
+            tensor[i][self.characters.index(ascii_char)] = 1
+        return tensor
+
+    def _pad_seq(self, samples):
+        seq, label = zip(*samples)
+        seq_lens = [len(s) for s in seq]
+        sorted_list = sorted(zip(seq, label, seq_lens), key=lambda x: -x[2])
+        seq, label, seq_lens = zip(*sorted_list)
+        seq = pad_sequence(seq)
+        return seq, torch.tensor(label), torch.tensor(seq_lens)
+
+    def pack_seq(self, samples):
+        seqs, labels, seq_lens = self._pad_seq(samples)
+        seqs = pack_padded_sequence(seqs, seq_lens)
+        return seqs, labels, seq_lens
+
+data_dir = Path("../processed_data/preprocess/bioc/propose_on_bioc/")
+sf_eval = SF([data_dir / "SH"])
+sf_train = SF([data_dir / "Ab3P", data_dir / "bioadi", data_dir / "medstract"], exclude=set(sf_eval.data["seq"]))
+
+dataset = SFLFPairs("test")
+dataloader = DataLoader(dataset, batch_size=4, shuffle=True,
+                        num_workers=1, collate_fn=pad_seq)
+i, (labels, padded_seqs, seq_lens) = next(enumerate(dataloader))
