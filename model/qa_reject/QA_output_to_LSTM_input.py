2eb1233ff6eabb26e065975127a1a72b1fec7049
Boxiang Liu <jollier.liu@gmail.com>
2020-11-25 15:07:17 -0800

    small fix

--- model/qa_reject/QA_output_to_LSTM_input.py
+++ model/qa_reject/QA_output_to_LSTM_input.py
@@ -6,8 +6,8 @@ for line in tqdm(sys.stdin):
     if line.startswith("  "):
         split_line = line.strip().split("|")
         if split_line[3] == "input":
-            valid_sf, gold_lf = 1, split_line[1] \
-                if float(split_line[2]) == 1.0 else 0, "none"
+            valid_sf, gold_lf = (1, split_line[1]) \
+                if float(split_line[2]) == 1.0 else (0, "none")
         else:
             sf, lf = split_line[0], split_line[1]
             correct_lf = int(lf == gold_lf)
