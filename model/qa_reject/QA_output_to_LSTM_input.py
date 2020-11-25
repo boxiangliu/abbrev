85613a38470bbdc9e4acfa392d2d072044a91c13
Boxiang Liu <jollier.liu@gmail.com>
2020-11-25 14:52:26 -0800

    new file:   model/qa_reject/QA_output_to_LSTM_input.py

---
+++ model/qa_reject/QA_output_to_LSTM_input.py
@@ -0,0 +1,24 @@
+import sys
+from tqdm import tqdm
+
+for line in tqdm(sys.stdin):
+    if line.startswith("  "):
+        split_line = line.strip().split("|")
+        if split_line[3] == "input":
+            if int(split_line[2]) == 1:
+                valid_sf = 1
+                gold_lf = split_line[1]
+            else: 
+                valid_sf = 0
+                gold_lf = "none"
+        else:
+            sf = split_line[0]
+            lf = split_line[1]
+            if lf == gold_lf:
+                correct_lf = 1
+            out_line = f"{sf}\t{lf}\t{valid_sf}\t{correct_lf}\n"
+            sys.stdout.write(out_line)
+        
+
+
+
