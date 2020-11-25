01f7745930696ad7f32d9808ea5eb185be9a4650
Boxiang Liu <jollier.liu@gmail.com>
2020-11-25 15:11:29 -0800

    small fix

--- model/qa_reject/QA_output_to_LSTM_input.py
+++ model/qa_reject/QA_output_to_LSTM_input.py
@@ -1,15 +1,17 @@
 import sys
 from tqdm import tqdm
 
-sys.stdout.write(f"sf\tlf\tgood_sf\tgood_lf\n")
+sys.stdout.write(f"sf\tlf\tgood_sf\tgood_lf\tgold_answer\n")
 for line in tqdm(sys.stdin):
     if line.startswith("  "):
         split_line = line.strip().split("|")
         if split_line[3] == "input":
             valid_sf, gold_lf = (1, split_line[1]) \
                 if float(split_line[2]) == 1.0 else (0, "none")
+            gold_answer = 1
         else:
             sf, lf = split_line[0], split_line[1]
             correct_lf = int(lf == gold_lf)
-            out_line = f"{sf}\t{lf}\t{valid_sf}\t{correct_lf}\n"
-            sys.stdout.write(out_line)
+            gold_answer = 0
+        out_line = f"{sf}\t{lf}\t{valid_sf}\t{correct_lf}\t{gold_answer}\n"
+        sys.stdout.write(out_line)
