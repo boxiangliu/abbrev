e9795828f6ff0de231aa9a7d1e4c6785418e09ff
Boxiang Liu <jollier.liu@gmail.com>
2020-12-07 16:02:41 -0800

    fixed finite state machine error

--- model/qa_to_ab3p/parse_ab3p_output.py
+++ model/qa_to_ab3p/parse_ab3p_output.py
@@ -5,18 +5,14 @@ sys.stdout.write("sf\tlf\tab3p\n")
 
 for line in sys.stdin:
     if line.startswith(">"):
-        if prev_state == "ab3p":
-            continue
-        elif prev_state == "squad":
+        if prev_state == "squad":
             sys.stdout.write(f"{sf}\t{lf}\t0\n")
 
         pmid, type_, sent_no = line.replace(">", "").strip().split("|")
         prev_state = "header"
 
     elif line.startswith("squad:"):
-        if prev_state == "ab3p":
-            continue
-        elif prev_state == "squad":
+        if prev_state == "squad":
             sys.stdout.write(f"{sf}\t{lf}\t0\n")
 
         lf, sf = line.strip().split("\t")[1:3]
