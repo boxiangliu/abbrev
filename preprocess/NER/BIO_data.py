66646ac083223a39d663eb680c9f843e8ebe5569
Boxiang Liu <jollier.liu@gmail.com>
2020-12-21 14:40:09 -0800

    updated preprocess/NER/BIO_data.py

--- preprocess/NER/BIO_data.py
+++ preprocess/NER/BIO_data.py
@@ -16,18 +16,33 @@ def init_container():
     return {"PSF": [], "PLF": [], "SF": [], "LF": [], "pmid": "", "type": ""}
 
 
+def reset_container():
+    for key in ["PSF", "PLF", "SF", "LF"]:
+        container[key] = []
+
+
 def parse_line(line, container):
     if line.startswith("id:"):
         container["pmid"] = line.strip().split("\t")[1]
     elif line.startswith("annotation:"):
-        entry_type, text = line.strip().split("\t")[1:3]
-        entry_type = entry_type.strip("0123456789")
-        text = text.strip()
-        container[entry_type].append(text)
+        parse_annotation_line(line, container)
     else:
         if line.startswith("text:"):
             update_text_type(container)
         write_BIO_data(container)
+        reset_container(container)
+
+
+def parse_annotation_line(line, container):
+    split_line = line.strip().split("\t")
+    if len(split_line) >= 3
+        entry_type, text = split_line[1:3]
+    elif len(split_line) == 2:
+        entry_type, text = split_line[1], ""
+
+    entry_type = entry_type.strip("0123456789")
+    text = text.strip()
+    container[entry_type].append(text)
 
 
 def update_text_type(container):
@@ -69,6 +84,8 @@ def write_positive_BIO_instance(PSF, PLF, SF, LF, pmid, text_type):
     length = len(LF)
     PLF_char_labels = make_PLF_char_labels(PLF, start_char, length)
     PLF_word_labels = char_to_word_labels(PLF, PLF_char_labels)
+    PLF_char_labels = ",".join(PLF_char_labels)
+    PLF_word_labels = ",".join(PLF_word_labels)
     PSF_label = 1
     sys.stdout.write(f"{PSF}\t{PLF}\t{PSF_label}\t{PLF_char_labels}\t{PLF_word_labels}\t{pmid}\t{text_type}\n")
 
@@ -94,8 +111,8 @@ def char_to_word_labels(text, char_labels):
 
 
 def write_negative_BIO_instance(PSF, PLF, pmid, text_type):
-    PLF_char_labels = ["O" for char in PLF]
-    PLF_word_labels = ["O" for word in PLF.split(" ")]
+    PLF_char_labels = ",".join(["O" for char in PLF])
+    PLF_word_labels = ",".join(["O" for word in PLF.split(" ")])
     PSF_label = 0
     sys.stdout.write(f"{PSF}\t{PLF}\t{PSF_label}\t{PLF_char_labels}\t{PLF_word_labels}\t{pmid}\t{text_type}\n")
 
