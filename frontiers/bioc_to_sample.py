e84b3d5084837fbb5a57a9b0c8be54c5b048b582
Boxiang Liu <jollier.liu@gmail.com>
2021-08-22 12:27:00 -0700

    debug

--- frontiers/bioc_to_sample.py
+++ frontiers/bioc_to_sample.py
@@ -1,11 +1,13 @@
 import sys
 for line in sys.stdin:
-    breakpoint()
     if line.startswith("text"):
         text = line.strip().split("\t")[1]
+        print(text)
     elif line.startswith("annotation\tSF"):
         sf = line.strip().split("\t")[2]
+        print(sf)
     elif line.startswith("annotation\tLF"):
         lf = line.strip().split("\t")[2]
+        print(lf)
         sys.stdout.write("{}\t{}\t{}\n".format(lf, sf, text))
 
